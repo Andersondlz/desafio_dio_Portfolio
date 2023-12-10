5895845ee136400f2ff0ef452055bab8831db8df
Andersondlz <andersondlz2009@gmail.com>
2023-12-10 13:05:08 -0300

    Atualização do Json e html

--- assest/js/main.js
+++ assest/js/main.js
@@ -32,10 +32,21 @@ function updateHardSkills(profileData){
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill =>`<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
-
 function updateLanguages(profileData){
     const languages = document.getElementById('profile.languages')
     languages.innerHTML = profileData.languages.map(languages => `<li>${languages}</li>`).join('')
+}
+
+function updatePortfolio(profileData){
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_black">${project.url}</a> 
+            </li>
+        `
+    }).join('')
 }
 
 (async () => { 
@@ -44,5 +55,6 @@ function updateLanguages(profileData){
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortfolio(profileData)
 })()
 
